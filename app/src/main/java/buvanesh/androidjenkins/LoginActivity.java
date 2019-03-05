d18c50b975a6b68d1f91ad94754062b1fd2b0818
Buvaneshkumar <36222772+kumarp777804@users.noreply.github.com>
2019-03-05 13:29:10 +0530

    Update LoginActivity.java

--- app/src/main/java/buvanesh/androidjenkins/LoginActivity.java
+++ app/src/main/java/buvanesh/androidjenkins/LoginActivity.java
@@ -1,9 +1,13 @@
 package buvanesh.androidjenkins;
 
-import android.os.Bundle;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class LoginActivity extends AppCompatActivity {
